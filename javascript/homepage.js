3ca0f200399988d8d29c7098c75ce23474107383
Jonathan Pynes <jonpynes@gmail.com>
2021-06-04 14:09:16 -0400

    javascript is broken

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,9 +1,14 @@
 // hamburger menu and nav
 const overlay = document.querySelector(".nav-ul");
 
-document.querySelector(".hamburger").onclick = () =>
-  overlay.classList.toggle("hidden");
-
+function myOverlay() {
+  document.querySelector(".hamburger").onclick = () =>
+    overlay.classList.toggle("hidden");
+}
 let links = document.querySelectorAll(".nav-ul-li-link");
 
-links.forEach((link) => link.addEventListener("click", overlay));
+function myOverlay() {
+  myOverlay(this);
+}
+
+links.forEach((link) => link.addEventListener("click", myOverlay));
