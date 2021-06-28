1a58c20d21459bd08959dc89689af72f2780eb82
Jonathan Pynes <jonpynes@gmail.com>
2021-06-28 17:59:28 -0400

    added some JS added some responsive styling... need to build active projects page and past projects page

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -9,3 +9,14 @@ function hideLink() {
 }
 let links = document.querySelectorAll(".nav-ul-li-link");
 links.forEach((link) => link.addEventListener("click", hideLink));
+
+//under construction alert
+
+function excuseMyMess() {
+  alert("Excuse my mess - still working on this portion");
+}
+
+let handleExcuseMyMess = document.querySelectorAll(".project-image");
+handleExcuseMyMess.forEach((handleExcuseMyMess) =>
+  handleExcuseMyMess.addEventListener("click", excuseMyMess)
+);
